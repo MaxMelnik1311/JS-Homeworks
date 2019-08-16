9cd98fc1d80f268e52b6362a25bdc184123bf945
MaxMelnik1311 <maaxfresh1100@gmail.com>
2019-08-16 22:12:18 +0300

    changes

--- hw-9/notepad-boilerplate-homework-09/src/js/view/renderNoteList.js
+++ hw-9/notepad-boilerplate-homework-09/src/js/view/renderNoteList.js
@@ -1,5 +1,7 @@
 function renderNoteList(listRef, notes) {
-    const elem = listRef.cloneNode(false);
-    elem.append(...notes.map(e => createListItem(e)));
-    listRef.replaceWith(elem);
+    // const elem = listRef.cloneNode(false);
+    // elem.append(...notes.map(e => createListItem(e)));
+    // listRef.replaceWith(elem);
+    listRef.innerHTML = '';
+    listRef.append(...notes.map(e => createListItem(e)));
 }
